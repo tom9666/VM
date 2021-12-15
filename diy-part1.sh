418598cd8d8dd75af4ac3e485bc44d2a495b8587
tom9666 <90450434+tom9666@users.noreply.github.com>
2021-12-15 14:03:22 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,9 +13,6 @@
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-git clone https://github.com/jerrykuku/lua-maxminddb.git /lede/package/lean/  
-git clone https://github.com/jerrykuku/luci-app-vssr.git /lede/package/lean/  
-
 # Add a feed source
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
