1e355458397c5900f5fa178564e68dc71b67c351
tom9666 <90450434+tom9666@users.noreply.github.com>
2021-12-15 12:48:08 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,4 +12,3 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.3.10/g' package/base-files/files/bin/config_generate
-sed -i 's/^\s*$[(]call\sEnsureVendoredVersion/#&/' ~/lede/feeds/packages/utils/dockerd/Makefile
